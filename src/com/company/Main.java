d39501088edce230412839421081f32849704f23
BuildTools <unconfigured@null.spigotmc.org>
2019-01-02 21:07:22 -0500

    :^)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,97 @@ package com.company;
 import java.util.*;
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int gameEnd = 0;
+        int turnCount = 0;
+        int row = -1;
+        int column = -1;
+        int[] boardPos = new int[2];
+        boolean cheatDetected = false;
         String board[][] = new String[][] {
-                {"[]","[]","[]"},
-                {"[]","[]","[]"},
-                {"[]","[]","[]"}
+                {"[1]","[2]","[3]"},
+                {"[4]","[5]","[6]"},
+                {"[7]","[8]","[9]"}
         };
-
-        //System.out.println(TicTacPrint.printBoard(board));
         Scanner stringReader = new Scanner(System.in);
-        System.out.println("You (X's) are playing Tic Tac Toe against a computer (O's). \n ");
+        System.out.println("You (X's) are playing Tic Tac Toe against a computer (O's).\n" +
+                "Who should go first? Type in \"me\" to go first, else type \"computer\" to let the computer go first.");
+        String whoFirst = stringReader.nextLine();
+        if (whoFirst.equalsIgnoreCase("me")){
+            System.out.println("You are going first.");
+        }
+        else if (whoFirst.equalsIgnoreCase("computer")){
+            turnCount++;
+            System.out.println("The computer will go first.");
+            //Computer Turn
+            System.out.println(TicTacPrint.printBoard(board));
+            System.out.println("Waiting for computer...");
+            Thread.sleep(3000);
+            if (turnCount == 1){
+                boardPos = TicTacPrint.firstComputerMove(board);
+            }
+            else {
+                boardPos = TicTacPrint.computerMove(board);
+            }
+            row = boardPos[0];
+            column = boardPos[1];
+            board[row][column] = "[O]";
+            System.out.println("computer tried to place in " + row + " row, and " + column + " column.");
+
+        }
+        else {
+            System.out.println("Sorry, I do not understand what you mean.");
+            System.exit(0);
+        }
+
+        System.out.println("Please type in which position you would like to place your [X] in.\n" +
+                "(Ex: '5');");
+
+        System.out.println();
+
+        while (gameEnd == 0) {
+            turnCount++;
+            cheatDetected = false;
+            while(!cheatDetected) {
+                //Player Turn
+                System.out.println("It's your turn. \n");
+                System.out.println(TicTacPrint.printBoard(board));
+                int playerPos = stringReader.nextInt();
+                boardPos = TicTacPrint.convertIntToBoardPos(playerPos);
+                row = boardPos[0];
+                column = boardPos[1];
+                if (board[row][column].equals("[X]") || board[row][column].equals("[O]")) {
+                    System.out.println("You definitely already picked that spot, try again.");
+                }
+                else {
+                    board[row][column] = "[X]";
+                    break;
+                }
+            }
 
 
+
+            //Computer Turn
+            System.out.println(TicTacPrint.printBoard(board));
+            System.out.println("Waiting for computer...");
+            Thread.sleep(3000); //computer is spending time in its complex algorithm :)
+            if (turnCount == 1){
+                boardPos = TicTacPrint.firstComputerMove(board);
+            }
+            else {
+                boardPos = TicTacPrint.computerMove(board);
+            }
+            row = boardPos[0];
+            column = boardPos[1];
+            board[row][column] = "[O]";
+            System.out.println("computer tried to place in " + row + " row, and " + column + " column.");
+
+
+            System.out.println(TicTacPrint.checkWin(board));
+        }
     }
 }
--- src/com/company/TicTacPrint.java
+++ src/com/company/TicTacPrint.java
@@ -12,12 +12,86 @@ public class TicTacPrint {
 
         return
                         a[0][0] + " | " + a[1][0] + " | " + a[2][0] + "\n" +
-                        "-------------" + "\n" +
+                        "----------------" + "\n" +
                         a[0][1] + " | " + a[1][1] + " | " + a[2][1] + "\n" +
-                        "-------------" + "\n" +
+                        "----------------" + "\n" +
                         a[0][2] + " | " + a[1][2] + " | " + a[2][2] + "\n"
                 ;
 
 
     }
+
+    static int boardPos[] = new int[2];
+    public static int[] convertIntToBoardPos(int b){
+        if (b == 1){
+            boardPos[0] = 0; //columns
+            boardPos[1] = 0; //rows
+        }
+        else if (b == 2){
+            boardPos[0] = 0;
+            boardPos[1] = 1;
+        }
+        else if (b == 3){
+            boardPos[0] = 0;
+            boardPos[1] = 2;
+        }
+        else if (b == 4){
+            boardPos[0] = 1;
+            boardPos[1] = 0;
+        }
+        else if (b == 5){
+            boardPos[0] = 1;
+            boardPos[1] = 1;
+        }
+        else if (b == 6){
+            boardPos[0] = 1;
+            boardPos[1] = 2;
+        }
+        else if (b == 7){
+            boardPos[0] = 2;
+            boardPos[1] = 0;
+        }
+        else if (b == 8){
+            boardPos[0] = 2;
+            boardPos[1] = 1;
+        }
+        else if (b == 9){
+            boardPos[0] = 2;
+            boardPos[1] = 2;
+        }
+        return boardPos;
+    }
+
+
+    public static int[] firstComputerMove(String[][] c) {
+        //Computer prioritizes first move in top left, if its taken it will go middle
+        if (c[0][0].equals("[X]") && c[1][1].equals("[5]")){
+            boardPos[0] = 1;
+            boardPos[1] = 1;
+        }
+        else if (c[0][0].equals("[1]")){
+            boardPos[0] = 0;
+            boardPos[1] = 0;
+        }
+        return boardPos;
+    }
+    static int horizCount = 0;
+    public static int[] computerMove(String[][] c){
+
+
+
+
+
+        return boardPos;
+    }
+
+
+  /*  public static String checkWin(String[][] d){
+        for (int i = 0; i < ; i++) {
+            
+        }
+        else {
+            return "no";
+        }
+    }*/
 }
